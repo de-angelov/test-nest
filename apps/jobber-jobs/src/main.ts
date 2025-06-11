341af5b3ac7db97d6c721d8c45930a6d71a8e1ad
Denis Angelov <denis.angelov@dataart.com>
2025-06-11 18:28:15 +0300

    new jobs microservice app

---
+++ apps/jobber-jobs/src/main.ts
@@ -0,0 +1,28 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+
+  app
+    .useGlobalPipes(new ValidationPipe({ whitelist: true }))
+    .setGlobalPrefix(globalPrefix)
+    .use(cookieParser());
+
+  const port = app.get(ConfigService).getOrThrow('PORT');
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
