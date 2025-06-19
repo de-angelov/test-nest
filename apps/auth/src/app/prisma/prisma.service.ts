5f21468d8c6a7fcb760b5d95564c19ebce156d28
Denis Angelov <denis.angelov@dataart.com>
2025-06-19 10:19:22 +0300

    rework project sructure

---
+++ apps/auth/src/app/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma-clients/auth';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+    // establish connections to db based on connection details provided in data source
+  }
+}
