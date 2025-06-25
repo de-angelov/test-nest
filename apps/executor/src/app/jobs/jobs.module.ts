3e14d72aa97642503c9e1b2373bde66161822f6c
Denis Angelov <denis.angelov@dataart.com>
2025-06-25 15:48:16 +0300

    prisma debian

--- apps/executor/src/app/jobs/jobs.module.ts
+++ apps/executor/src/app/jobs/jobs.module.ts
@@ -1,8 +1,6 @@
 import { PulsarModule } from 'lib/pulsar/src/lib/pulsar.module';
 import { Module } from '@nestjs/common';
 import { FibonacciConsumer } from './fibonacci/fibonacci.consumer';
-
-// test
 @Module({
   imports: [PulsarModule],
   providers: [FibonacciConsumer],
--- lib/pulsar/src/lib/pulsar.client.ts
+++ lib/pulsar/src/lib/pulsar.client.ts
@@ -12,7 +12,6 @@ export class PulsarClient implements OnModuleDestroy {
   private readonly consumers: Consumer[] = [];
 
   constructor(private readonly configService: ConfigService) {}
-  // test
   async onModuleDestroy() {
     for (const producer of this.producers) {
       await producer.close();
