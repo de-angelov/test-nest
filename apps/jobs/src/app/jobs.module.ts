ddc17105934cfb40e824d78e56890816153c0060
Denis Angelov <denis.angelov@dataart.com>
2025-06-26 21:42:23 +0300

    more services

--- apps/jobs/src/app/jobs.module.ts
+++ apps/jobs/src/app/jobs.module.ts
@@ -3,21 +3,30 @@ import { FibonacciJob } from './jobs/fibonacci/fibonacci.job';
 import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientOptions, ClientsModule, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
 import { join } from 'path';
 import { PulsarModule } from '@jobber/pulsar';
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     DiscoveryModule,
     PulsarModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(__dirname, '../../lib/grpc/proto/auth.proto'),
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          const config: ClientOptions = {
+            transport: Transport.GRPC,
+            options: {
+              url: configService.getOrThrow('AUTH_GRPC_SERVICE_URL'),
+              package: AUTH_PACKAGE_NAME,
+              protoPath: join(__dirname, '../../lib/grpc/proto/auth.proto'),
+            },
+          };
+
+          return config;
         },
       },
     ]),
