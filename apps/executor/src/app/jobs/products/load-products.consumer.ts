37f7a0cd9a7d0084595e93568d2bb1de80578f18
Denis Angelov <denis.angelov@dataart.com>
2025-06-29 16:44:54 +0300

    products grpcs client

--- apps/executor/src/app/jobs/products/load-products.consumer.ts
+++ apps/executor/src/app/jobs/products/load-products.consumer.ts
@@ -1,19 +1,42 @@
+import {
+  Packages,
+  PRODUCTS_SERVICE_NAME,
+  ProductsServiceClient,
+} from '@jobber/grpc';
 import { Jobs } from '@jobber/nestjs';
 import {
   LoadProductsMessage,
   PulsarClient,
   PulsarConsumer,
 } from '@jobber/pulsar';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class LoadProductsConsumer extends PulsarConsumer<LoadProductsMessage> {
-  constructor(pulsarClient: PulsarClient) {
+export class LoadProductsConsumer
+  extends PulsarConsumer<LoadProductsMessage>
+  implements OnModuleInit
+{
+  private productsService: ProductsServiceClient;
+
+  constructor(
+    pulsarClient: PulsarClient,
+    @Inject(Packages.PRODUCTS) private clientGrpc: ClientGrpc
+  ) {
     super(pulsarClient, Jobs.LOAD_PRODUCTS);
   }
-  protected onMessage(data: LoadProductsMessage) {
-    console.log(data);
 
-    return Promise.resolve();
+  async onModuleInit(): Promise<void> {
+    this.productsService = this.clientGrpc.getService(PRODUCTS_SERVICE_NAME);
+
+    super.onModuleInit();
+  }
+
+  protected async onMessage(data: LoadProductsMessage) {
+    console.log('On message: ', data);
+    const createObs = this.productsService.createProduct(data);
+
+    await firstValueFrom(createObs);
   }
 }
--- apps/executor/src/app/jobs/products/load-products.module.ts
+++ apps/executor/src/app/jobs/products/load-products.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { LoadProductsConsumer } from './load-products.consumer';
 import { PulsarModule } from '@jobber/pulsar';
+import { ClientOptions, ClientsModule, Transport } from '@nestjs/microservices';
+import { Packages } from '@jobber/grpc';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+
+const getProductsConfig = (configService: ConfigService) => {
+  const config: ClientOptions = {
+    transport: Transport.GRPC,
+    options: {
+      url: configService.getOrThrow('PRODUCTS_GRPC_SERVICE_URL'),
+      package: Packages.PRODUCTS,
+      protoPath: join(__dirname, '../../lib/grpc/proto/products.proto'),
+    },
+  };
+
+  return config;
+};
 
 @Module({
-  imports: [PulsarModule],
+  imports: [
+    PulsarModule,
+    ClientsModule.registerAsync([
+      {
+        name: Packages.PRODUCTS,
+        inject: [ConfigService],
+        useFactory: getProductsConfig,
+      },
+    ]),
+  ],
   providers: [LoadProductsConsumer],
 })
 export class LoadProductsModule {}
