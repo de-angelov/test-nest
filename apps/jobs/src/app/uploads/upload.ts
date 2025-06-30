9b3fc45b6dac689ea4f7257e09049928a68647ad
Denis Angelov <denis.angelov@dataart.com>
2025-06-30 17:41:54 +0300

    volumes

--- apps/jobs/src/app/uploads/upload.ts
+++ apps/jobs/src/app/uploads/upload.ts
@@ -1,3 +1,3 @@
 import path = require('path');
 
-export const UPLOAD_FILE_PATH = path.join(process.cwd(), 'app/jobs/uploads');
+export const UPLOAD_FILE_PATH = path.join(process.cwd(), 'apps/jobs/uploads');
